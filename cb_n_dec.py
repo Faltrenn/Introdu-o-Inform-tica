ea75fde158f8d92d398a741d6705fc8d87c593eb
Faltrenn <emanuel.alves.702@ufrn.edu.br>
2023-04-05 14:19:57 -0300

    Separação do código em funções, melhoria nas instruções e passo a passo adicionado.

--- cb_n_dec.py
+++ cb_n_dec.py
@@ -1,13 +1,5 @@
 ALGARISMOS_ATE_HEXA = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F")
-
-print("Como funciona:\n")
-
-print("Digite o número e a base que ele está: <número> <base>\n")
-
-print("Exemplo: 100 2\n")
-
-print("Pressione o ENTER sem digitar nada, para sair do programa.\n")
-
+    
 
 def converter_digito(digito: str) -> int:
     return ALGARISMOS_ATE_HEXA.index(digito.upper())
@@ -18,6 +10,35 @@ def validar_numero(numero: str, base: int) -> bool:
             return False
     return True
 
+def converter_numero(numero: str, base: int) -> int:
+    tamanho_do_numero = len(numero) - 1
+    resultado = 0
+
+    resolucao = "\n"
+    for indice, digito in enumerate(numero):
+        resultado += converter_digito(digito) * (base ** (tamanho_do_numero - indice))
+        resolucao += f"{digito} * {base}^{tamanho_do_numero - indice} {'+ ' if indice != tamanho_do_numero else ''}"
+
+    print(f"{resolucao} = {resultado}\n")
+
+    print(f"{numero} na base {base} é {resultado} na base 10\n")
+
+
+def boas_vindas():
+    print("Como funciona:\n")
+
+    print("Digite o número e a base que ele está: <número> <base>\n")
+
+    print("Exemplo:\n")
+
+    print("Digite o número e a base que ele está: 200 4")
+
+    converter_numero("200", 4)
+
+    print("Pressione o ENTER sem digitar nada, para sair do programa.\n")
+
+
+boas_vindas()
 
 escolha: str = None
 
@@ -25,11 +46,6 @@ while escolha != "":
     escolha = input("Digite o número e a base que ele está: ")
     partes = escolha.split(" ")
     if validar_numero(partes[0], int(partes[1])):
-        tamanho_do_numero = len(partes[0]) - 1
-        base = int(partes[1])
-        resultado = 0
-        for indice, digito in enumerate(partes[0]):
-            resultado += converter_digito(digito) * (base ** (tamanho_do_numero - indice))
-        print(f"{partes[0]} na base {partes[1]} é {resultado} na base 10")
+        converter_numero(partes[0], int(partes[1]))
     else:
-        print(f"{partes[0]} é inválido na base {partes[1]}")
+        print(f"{partes[0]} é inválido na base {partes[1]}\n")
