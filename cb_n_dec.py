6dfa8bf176df062306eec661dc15db58829f29ac
Faltrenn <emanuel.alves.702@ufrn.edu.br>
2023-04-08 01:16:13 -0300

    Adição de cores para melhor leitura. Bug das instruções não mostra a solução resolvido. Bug de não aceitar números com algarismos de base acima da 10 resolvido.

--- cb_n_dec.py
+++ cb_n_dec.py
@@ -1,4 +1,20 @@
+import os
+
+os.system("color") #Necessário para que os códigos de cores funcionem no Windows
+
 ALGARISMOS = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+
+class Cores:
+    VERDE = "\033[1;32m"
+    MAGENTA = "\033[1;35m"
+    CIANO = "\033[1;36m"
+    AZUL = "\033[1;34m"
+    RESET = "\033[0;0m"
+
+
+def colorir(texto: any, cor: str) -> str:
+    return f"{cor}{texto}{Cores.RESET}"
 
 
 def converter_digito(digito: str) -> int:
@@ -14,9 +30,9 @@ def validar_partes(partes: list) -> bool:
     if len(partes) != 2:
         print("\nDigite o número e a base que ele está: <número> <base>\n")
         return False
-    numero = partes[0]
-    base = partes[1]
-    if not numero.isnumeric():
+    numero: str = partes[0]
+    base: str = partes[1]
+    if not numero.isalnum():
         print("\nDigite um número válido\n")
         return False
     if not base.isnumeric() or int(base) < 2:
@@ -34,9 +50,10 @@ def converter_numero(numero: str, base: int) -> int:
     resolucao = "\n"
     for indice, digito in enumerate(numero):
         resultado += converter_digito(digito) * (base ** (tamanho_do_numero - indice))
-        resolucao += f"{digito} * {base}^{tamanho_do_numero - indice} {'+ ' if indice != tamanho_do_numero else ''}"
+        resolucao += f"{digito if digito.isnumeric() else colorir(converter_digito(digito), Cores.AZUL)} * {base}^{tamanho_do_numero - indice} {'+ ' if indice != tamanho_do_numero else ''}"
 
-    return resultado, resolucao
+    print(f"{resolucao}= {resultado}\n")
+    print(f"{colorir(numero, Cores.VERDE)} na base {colorir(base, Cores.MAGENTA)} é {colorir(resultado, Cores.CIANO)} na base 10\n")
 
 
 print("Como funciona:\n")
@@ -74,9 +91,6 @@ while True:
     base = int(base)
 
     if validar_numero(numero, base):
-        resultado, resolucao = converter_numero(numero, base)
-        print(f"{resolucao}= {resultado}\n")
-
-        print(f"{numero} na base {base} é {resultado} na base 10\n")
+        converter_numero(numero, base)
     else:
         print(f"{numero} é inválido na base {base}\n")
